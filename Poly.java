8602da0094c6efbb44729e1946171e7a057e38cd
durga1808 <137372215+durga1808@users.noreply.github.com>
2023-07-20 15:46:43 +0530

    Update Poly.java

--- Poly.java
+++ Poly.java
@@ -5,7 +5,7 @@ class Splendor extends Poly{
   void run(){System.out.println("running safely with 60km");}  
   
   public static void main(String args[]){  
-    Bike b = new Splendor();//upcasting  
-    b.run();  
+    Poly p = new Splendor();//upcasting  
+    p.run();  
   }  
 }  
