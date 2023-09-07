99ad69be9b5f402003a68d754038a2be71d581ae
Anushiya <anushiyadhamu@gmail.com>
2023-09-07 16:23:50 +0530

    created consumer for trace

--- src/main/java/com/zaga/kafka/producer/TraceSerializer.java
+++ src/main/java/com/zaga/kafka/producer/TraceSerializer.java
@@ -1,27 +1,31 @@
-// package com.zaga.kafka.producer;
+package com.zaga.kafka.producer;
 
-// import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zaga.entity.oteltrace.OtelTrace;
 
-// public class TraceSerializer  implements Serializer<OtelTrace> {
+import org.apache.kafka.common.serialization.Serializer;
+
+public class TraceSerializer  implements Serializer<OtelTrace> {
+
+    private final ObjectMapper objectMapper;
+
+    public TraceSerializer() { 
+        this.objectMapper = new ObjectMapper();
+    }
+
+    @Override
+    public byte[] serialize(String topic, OtelTrace trace) {
+        try {
+            return objectMapper.writeValueAsBytes(trace);
+        } catch (Exception e) {
+            throw new RuntimeException("Error serializing to JSON", e);
+        }
+    }
+
+    @Override
+    public void close() {
+
+    }
     
-// }
-//  private final objectMapper objectMapper;
-
-//     public KafkaSerializer() {
-//         this.objectMapper = new ObjectMapper();
-//     }
-
-//     @Override
-//     public byte[] serialize(String topic, ProductDetails productDetails) {
-//         try {
-//             return objectMapper.writeValueAsBytes(productDetails);
-//         } catch (Exception e) {
-//             throw new RuntimeException("Error serializing to JSON", e);
-//         }
-//     }
-
-//     @Override
-//     public void close() {
-//         // No resources to close for this serializer
-//     }
-// }
+}
+ 
