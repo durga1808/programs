a5b7c6b54d36c78270a5945f5d1059903ee1f8a1
umamuthu94 <umamuthu1994@gmail.com>
2023-09-14 16:43:57 +0530

    created fetch api for log

--- src/main/java/com/zaga/controller/LogController.java
+++ src/main/java/com/zaga/controller/LogController.java
@@ -9,11 +9,13 @@ import com.zaga.entity.otellog.OtelLog;
 import com.zaga.handler.command.LogCommandHandler;
 import com.zaga.handler.query.LogQueryHandler;
 
+import io.quarkus.mongodb.panache.PanacheMongoEntityBase;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
@@ -47,5 +49,24 @@ public class LogController {
     public List<Document> getLogByServiceName(@QueryParam("serviceName") String serviceName){
         return logQueryHandler.getLogByServiceName(serviceName);
     }
+
+    @GET
+    @Path("/bySeverity/{severityText}")
+    public List<Document> getLogsBySeverityText(@QueryParam("severityText") String severityText) {
+        return logQueryHandler.getLogsBySeverityText(severityText);
+    }
+
+    @GET
+    @Path("/search/{serviceName}/{severityText}")
+    public List<Document> getLogsByServiceNameAndSeverityText(String serviceName, String severityText){
+        return logQueryHandler.getLogsByServiceNameAndSeverityText(serviceName, severityText);
+    }
+
+    @GET
+    @Path("/service/{serviceName}")
+    public   List<Document> aggregateDocuments(@PathParam("serviceName") String serviceName) {
+        return logQueryHandler.aggregateDocuments(serviceName);
+    }
+    
 }
 
