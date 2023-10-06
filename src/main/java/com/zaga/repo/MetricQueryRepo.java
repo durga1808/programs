bc0e41ec9533b259c9d0d307f31b032b273f40ac
Anushiya <anushiyadhamu@gmail.com>
2023-10-06 14:24:20 +0530

    updated the metricDTO

--- src/main/java/com/zaga/repo/MetricQueryRepo.java
+++ src/main/java/com/zaga/repo/MetricQueryRepo.java
@@ -16,13 +16,10 @@ public class MetricQueryRepo implements PanacheMongoRepository<MetricDTO> {
 public List<MetricDTO> getMetricData(int timeAgoMinutes, String serviceName) {
     LocalDateTime timeAgo = LocalDateTime.now().minusMinutes(timeAgoMinutes);
 
-    // Convert 'timeAgo' to a Date for comparison with the 'date' field
     Date timeAgoDate = Date.from(timeAgo.atZone(ZoneId.systemDefault()).toInstant());
 
-    // Query the database
-    PanacheQuery<MetricDTO> query = find("serviceName = ?1 and date >= ?2", serviceName, timeAgoDate);
 
-    // Execute the query and return the results as a list
+    PanacheQuery<MetricDTO> query = find("serviceName = ?1 and date >= ?2", serviceName, timeAgoDate);
     List<MetricDTO> result = query.list();
     return result;
 }
