462632fedd3aeddc462b1c954f0f5c81bd7ce466
Anushiya <anushiyadhamu@gmail.com>
2023-10-17 15:43:58 +0530

    updated the metric date wise

--- src/main/java/com/zaga/handler/MetricQueryHandler.java
+++ src/main/java/com/zaga/handler/MetricQueryHandler.java
@@ -1,14 +1,28 @@
 package com.zaga.handler;
 
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.bson.Document;
+import org.bson.conversions.Bson;
 
+import com.mongodb.client.AggregateIterable;
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.Filters;
 import com.zaga.entity.queryentity.metric.MetricDTO;
 import com.zaga.repo.MetricQueryRepo;
 
+import io.quarkus.mongodb.panache.PanacheQuery;
+import io.quarkus.panache.common.Parameters;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
@@ -25,10 +39,52 @@ public class MetricQueryHandler {
         return metricQueryRepo.listAll();
     }
 
-    public List<MetricDTO> getMetricData(LocalDate from,LocalDate to,String serviceName) {
-       List<MetricDTO> results = metricQueryRepo.getMetricData(from,to, serviceName);
-        return results;
-    }
+    // public List<MetricDTO> getMetricData(LocalDate from,LocalDate to,String serviceName) {
+    //    List<MetricDTO> results = metricQueryRepo.getMetricData(from,to, serviceName);
+    //     return results;
+    // }
+   
+public List<MetricDTO> getMetricData(LocalDate from, LocalDate to, String serviceName) {
+    Bson timeFilter = createCustomDateFilter(from, to);
+    Bson serviceNameFilter = Filters.eq("serviceName", serviceName);
 
+    Bson finalFilter = Filters.and(timeFilter, serviceNameFilter);
+
+    MongoCollection<Document> collection = mongoClient
+            .getDatabase("OtelMetric")
+            .getCollection("MetricDTO");
+
+    List<MetricDTO> filteredResults = new ArrayList<>();
+    
+    try (MongoCursor<Document> cursor = collection.find(finalFilter).iterator()) {
+        while (cursor.hasNext()) {
+            Document document = cursor.next();
+            MetricDTO metricDTO = convertDocumentToMetricDTO(document);
+            filteredResults.add(metricDTO);
+        }
+    }
+    
+    return filteredResults;
+}
+private MetricDTO convertDocumentToMetricDTO(Document document) {
+
+    MetricDTO metricDTO = new MetricDTO();
+
+    metricDTO.setDate(document.getDate("date")); 
+    metricDTO.setServiceName(document.getString("serviceName")); 
+    metricDTO.setCpuUsage(document.getDouble("cpuUsage"));
+    metricDTO.setMemoryUsage(document.getInteger("memoryUsage"));
+
+
+
+    return metricDTO;
+}
+
+private Bson createCustomDateFilter(LocalDate from, LocalDate to) {
+    return Filters.and(
+            Filters.gte("date", from.atStartOfDay()),
+            Filters.lt("date", to.plusDays(1).atStartOfDay())
+    );
+  }
 
 }
--- src/main/java/com/zaga/repo/MetricQueryRepo.java
+++ src/main/java/com/zaga/repo/MetricQueryRepo.java
@@ -1,18 +1,29 @@
 package com.zaga.repo;
 
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Sorts;
 import com.zaga.entity.queryentity.metric.MetricDTO;
 import io.quarkus.mongodb.panache.PanacheMongoRepository;
-import io.quarkus.mongodb.panache.PanacheQuery;
+import io.quarkus.panache.common.Parameters;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
 
 @ApplicationScoped
 public class MetricQueryRepo implements PanacheMongoRepository<MetricDTO> {
@@ -41,31 +52,64 @@ public class MetricQueryRepo implements PanacheMongoRepository<MetricDTO> {
     //     return Date.from(istDateTime.atZone(istZone).toInstant());
     // }
 
+    @Inject
+    MongoClient mongoClient;
 
-    public List<MetricDTO> getMetricData(LocalDate from, LocalDate to, String serviceName) {
-    // Calculate the start time in UTC
-    LocalDateTime startTimeUtc = LocalDateTime.now(ZoneOffset.UTC).minusMinutes(ChronoUnit.MINUTES.between(from.atStartOfDay(), to.atStartOfDay()));
-    Date startDateUtc = Date.from(startTimeUtc.toInstant(ZoneOffset.UTC));
+    // public List<MetricDTO> getMetricData(LocalDate from, LocalDate to, String serviceName) {
+    //     // Calculate the start time in UTC and end time in UTC
+    //     Instant fromUtc = from.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant();
+    //     Instant toUtc = to.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant();
 
-    // Query metrics with the given service name and start time
-    PanacheQuery<MetricDTO> query = find("serviceName = ?1 and date >= ?2", serviceName, startDateUtc);
-    List<MetricDTO> results = query.list();
+    //     // Create the aggregation pipeline stages
+    //     Document addFieldsStage = Document.parse("{$addFields: { justDate: { $dateToString: { format: '%m-%d-%Y', date: '$date' } } }}");
 
-    // Convert UTC dates to IST
-    results.forEach(metricDTO -> {
-        metricDTO.setDate(convertUtcToIst(metricDTO.getDate()));
-    });
+    //     Bson matchStage = Filters.and(
+    //         Filters.eq("serviceName", serviceName),
+    //         Filters.gte("justDate", from.toString()), // Convert LocalDate to string
+    //         Filters.lte("justDate", to.toString())     // Convert LocalDate to string
+    //     );
 
-    return results;
-}
+    //     // Build the aggregation pipeline
+    //     List<Bson> pipeline = List.of(addFieldsStage, matchStage);
 
-private Date convertUtcToIst(Date utcDate) {
-    Instant instant = utcDate.toInstant();
-    ZoneId istZone = ZoneId.of("Asia/Kolkata");
-    LocalDateTime istDateTime = LocalDateTime.ofInstant(instant, istZone);
-    return Date.from(istDateTime.atZone(istZone).toInstant());
-}
+    //     // Get the MongoDB collection for your entity
+    //     MongoCollection<MetricDTO> collection = mongoClient
+    //         .getDatabase("YourDatabaseName")
+    //         .getCollection("YourCollectionName", MetricDTO.class);
 
-    
-    
+    //     // Execute the aggregation pipeline
+    //     AggregateIterable<MetricDTO> aggregationResult = collection.aggregate(pipeline);
+
+    //     List<MetricDTO> results = new ArrayList<>();
+
+    //     try (MongoCursor<MetricDTO> cursor = aggregationResult.iterator()) {
+    //         while (cursor.hasNext()) {
+    //             results.add(cursor.next());
+    //         }
+    //     }
+
+    //     // Convert UTC dates to IST
+    //     results.forEach(metricDTO -> metricDTO.setDate(convertUtcToIst(metricDTO.getDate())));
+
+    //     return results;
+    // }
+    public List<MetricDTO> findDataByDateRange(LocalDate from, LocalDate to, String serviceName) {
+        // Calculate the start time in UTC and end time in UTC
+        Instant fromUtc = from.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant();
+        Instant toUtc = to.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant();
+
+        // Convert to Date
+        Date fromDate = Date.from(fromUtc);
+        Date toDate = Date.from(toUtc);
+
+        // Perform the date range query
+        return list("serviceName = ?1 and date >= ?2 and date <= ?3", serviceName, fromDate, toDate);
+    }
+
+    // private Date convertUtcToIst(Date utcDate) {
+    //     Instant instant = utcDate.toInstant();
+    //     ZoneId istZone = ZoneId.of("Asia/Kolkata");
+    //     LocalDateTime istDateTime = LocalDateTime.ofInstant(instant, istZone);
+    //     return Date.from(istDateTime.atZone(istZone).toInstant());
+    // }
 }
