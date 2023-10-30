3af302c868827585d96f7e4b61951ae42ec2c425
Anushiya <anushiyadhamu@gmail.com>
2023-10-30 14:22:59 +0530

    updated the dbmetric

--- src/main/java/com/zaga/entity/queryentity/trace/DBMetric.java
+++ src/main/java/com/zaga/entity/queryentity/trace/DBMetric.java
@@ -1,6 +1,15 @@
 package com.zaga.entity.queryentity.trace;
 
-public class DBMetric {
- private Long dbCallCount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DBMetric {
+ private String serviceName;
+ private Long dbCallCount;
+ private String dbName;
+ private Long dbPeakLatencyCount;
 }
