c2ad7b172d281137547bd5f311606b8d4b8523e6
umamuthu94 <umamuthu1994@gmail.com>
2023-11-16 17:10:38 +0530

    updated kepler api for get all and date and time based

--- src/main/java/com/zaga/controller/KeplerMetricController.java
+++ src/main/java/com/zaga/controller/KeplerMetricController.java
@@ -1,12 +1,45 @@
 package com.zaga.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zaga.entity.kepler.KeplerMetric;
 import com.zaga.handler.KeplerMetricHandler;
 
 import jakarta.inject.Inject;
-
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+@Path("/kepler")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class KeplerMetricController {
     
-
     @Inject
     KeplerMetricHandler keplerMetricHandler;
+
+@GET
+@Path("/getAllKepler")
+@Produces(MediaType.APPLICATION_JSON)
+public List<KeplerMetric> getLogMetricsCount(
+    ) {
+    return keplerMetricHandler.getAllKeplerData();
+}
+
+@GET
+    @Path("/getByserviceNameAndMinutesAgo")
+    public List<KeplerMetric> getKeplerByTimedased(
+        @QueryParam("from") LocalDate from,
+        @QueryParam("to") LocalDate to,
+        @QueryParam("minutesAgo") int minutesAgo
+    ) {
+       return keplerMetricHandler.getKeplerData(from,to, minutesAgo);
+                 
+}
+
 }
--- src/main/java/com/zaga/handler/KeplerMetricHandler.java
+++ src/main/java/com/zaga/handler/KeplerMetricHandler.java
@@ -1,5 +1,19 @@
 package com.zaga.handler;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.Filters;
+import com.zaga.entity.kepler.KeplerMetric;
+import com.zaga.entity.queryentity.trace.KafkaMetrics;
 import com.zaga.repo.KeplerMetricRepo;
 
 import jakarta.enterprise.context.ApplicationScoped;
@@ -11,4 +25,86 @@ public class KeplerMetricHandler {
 
     @Inject 
     KeplerMetricRepo keplerMetricRepo;
+
+     @Inject
+    MongoClient mongoClient;
+
+public List<KeplerMetric> getAllKeplerData() {
+        List<KeplerMetric> allKepler = keplerMetricRepo.listAll();
+        return allKepler;
+    }
+
+ public List<KeplerMetric> getKeplerData(LocalDate from, LocalDate to, int minutesAgo) {
+        Bson timeFilter;
+    
+        // Rearrange 'from' and 'to' if 'to' is earlier than 'from'
+        if (from != null && to != null && to.isBefore(from)) {
+            LocalDate temp = from;
+            from = to;
+            to = temp;
+        }
+    
+        if (from != null && to != null) {
+            timeFilter = createCustomDateFilter(from, to);
+        } else if (minutesAgo > 0) {
+            LocalDateTime currentDateTime = LocalDateTime.now();
+            LocalDateTime startOfToday = LocalDate.now().atStartOfDay();
+        
+            // Calculate 'fromDateTime' based on 'minutesAgo' but ensure it doesn't go beyond the start of today
+            LocalDateTime fromDateTime = currentDateTime.minusMinutes(minutesAgo);
+            if (fromDateTime.isBefore(startOfToday)) {
+                fromDateTime = startOfToday;
+            }
+        
+            // Set 'toDateTime' to the current time
+            LocalDateTime toDateTime = currentDateTime;
+        
+            timeFilter = Filters.and(
+                Filters.gte("date", fromDateTime),
+                Filters.lt("date", toDateTime)
+            );
+        }
+         else {
+            // Handle the case when neither date range nor minutesAgo is provided
+            throw new IllegalArgumentException("Either date range or minutesAgo must be provided");
+        }
+    
+        Bson finalFilter = Filters.and(timeFilter);
+    
+        MongoCollection<Document> collection = mongoClient
+            .getDatabase("KeplerMetric")
+            .getCollection("Metrics");
+    
+        List<KeplerMetric> filteredResults = new ArrayList<>();
+    
+        try (MongoCursor<Document> cursor = collection.find(finalFilter).iterator()) {
+            while (cursor.hasNext()) {
+                Document document = cursor.next();
+                KeplerMetric keplerMetric = convertDocumentToMetricDTO(document);
+                filteredResults.add(keplerMetric);
+            }
+        }
+    
+        return filteredResults;
+    }
+    
+private KeplerMetric convertDocumentToMetricDTO(Document document) {
+
+    KafkaMetrics keplerMetric = new KafkaMetrics();
+
+    keplerMetric.setServiceName(document.getString("serviceName")); 
+    keplerMetric.setKafkaCallCount(document.getLong("kafkaCallCount"));
+    keplerMetric.setKafkaPeakLatency(document.getLong("memoryUsage"));
+
+
+
+    return null;
+}
+
+private Bson createCustomDateFilter(LocalDate from, LocalDate to) {
+    return Filters.and(
+            Filters.gte("date", from.atStartOfDay()),
+            Filters.lt("date", to.plusDays(1).atStartOfDay())
+    );
+  }
 }
