58c965e7ca3a397bac5693ad503106db5b7ea374
Anushiya <anushiyadhamu@gmail.com>
2023-11-24 11:33:35 +0530

    updated the syslog

--- src/main/java/com/zaga/controller/KeplerMetricController.java
+++ src/main/java/com/zaga/controller/KeplerMetricController.java
@@ -1,25 +1,11 @@
 package com.zaga.controller;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.List;
-
-import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.zaga.entity.kepler.KeplerMetric;
 import com.zaga.entity.queryentity.kepler.KeplerMetricDTO;
-import com.zaga.entity.queryentity.kepler.KeplerMetricQuery;
 import com.zaga.entity.queryentity.kepler.Response.ContainerPowerMetrics;
 import com.zaga.entity.queryentity.kepler.Response.KeplerResponseData;
 import com.zaga.handler.KeplerMetricHandler;
 import com.zaga.repo.KeplerMetricRepo;
-
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
@@ -29,140 +15,173 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 
 @Path("/kepler")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class KeplerMetricController {
 
-    @Inject
-    KeplerMetricHandler keplerMetricHandler;
+  @Inject
+  KeplerMetricHandler keplerMetricHandler;
 
-    @Inject
-    KeplerMetricRepo keplerMetricRepo;
+  @Inject
+  KeplerMetricRepo keplerMetricRepo;
 
+  // @GET
+  // @Path("/getKeplerData")
+  // public List<KeplerMetricQuery> getKeplerByTimedased( ) {
+  //     return keplerMetricHandler.getKeplerData();
+  // }
 
-    @GET
-    @Path("/getKeplerData")
-    public List<KeplerMetricQuery> getKeplerByTimedased( ) {
-        return keplerMetricHandler.getKeplerData();
+  @POST
+  @Path("/addKeplerMock")
+  public KeplerMetricDTO addKeplerMetricDTO(
+    @RequestBody KeplerMetricDTO keplerMetricDTO
+  ) {
+    keplerMetricRepo.persist(keplerMetricDTO);
+    return keplerMetricDTO;
+  }
+
+  @GET
+  @Path("/getByTimeMock")
+  public Response getKeplerMetricByTime(
+    @QueryParam("minutesAgo") Integer minutesAgo
+  ) {
+    Instant currentInstant = Instant.now();
+    Instant minutesAgoInstant = currentInstant.minus(
+      minutesAgo,
+      ChronoUnit.MINUTES
+    );
+
+    List<KeplerMetricDTO> resDto = keplerMetricRepo
+      .find("date >= ?1", minutesAgoInstant)
+      .list();
+
+    List<String> uniqueServiceNamesList = new ArrayList<>();
+
+    for (KeplerMetricDTO metricEntry : resDto) {
+      if (!uniqueServiceNamesList.contains(metricEntry.getServiceName())) {
+        uniqueServiceNamesList.add(metricEntry.getServiceName());
+      }
     }
 
-    @POST
-    @Path("/addKeplerMock")
-    public KeplerMetricDTO addKeplerMetricDTO(@RequestBody KeplerMetricDTO keplerMetricDTO) {
-        keplerMetricRepo.persist(keplerMetricDTO);
-        return keplerMetricDTO;
+    List<KeplerResponseData> finalResponse = new ArrayList<>();
+
+    // Find By ServiceName from response
+    for (String serviceName : uniqueServiceNamesList) {
+      List<ContainerPowerMetrics> containerPowerMetricsList = new ArrayList<>();
+      for (KeplerMetricDTO entry : resDto) {
+        if (entry.getServiceName().equals(serviceName)) {
+          ContainerPowerMetrics containerPowerMetrics = new ContainerPowerMetrics(
+            entry.getDate(),
+            entry.getPowerConsumption()
+          );
+          containerPowerMetricsList.add(containerPowerMetrics);
+        }
+      }
+      KeplerResponseData keplerResponseData = new KeplerResponseData(
+        serviceName,
+        containerPowerMetricsList
+      );
+      finalResponse.add(keplerResponseData);
     }
 
-    @GET
-    @Path("/getByTimeMock")
-    public Response getKeplerMetricByTime(@QueryParam("minutesAgo") Integer minutesAgo) {
-        Instant currentInstant = Instant.now();
-        Instant minutesAgoInstant = currentInstant.minus(minutesAgo, ChronoUnit.MINUTES);
+    return Response.ok(finalResponse).build();
+  }
 
-        List<KeplerMetricDTO> resDto = keplerMetricRepo.find("date >= ?1", minutesAgoInstant).list();
+  @GET
+  @Path("/getAllKepler-MetricData")
+  public Response getAllKeplerMetricDatas(
+    @QueryParam("from") LocalDate from,
+    @QueryParam("to") LocalDate to,
+    @QueryParam("minutesAgo") int minutesAgo,
+    @QueryParam("type") String type,
+    @QueryParam("keplerType") List<String> keplerTypeList
+  ) {
+    LocalDateTime APICallStart = LocalDateTime.now();
 
-        List<String> uniqueServiceNamesList = new ArrayList<>();
+    System.out.println(
+      "------------API call startTimestamp------ " + APICallStart
+    );
 
-        for (KeplerMetricDTO metricEntry : resDto) {
-            if (!uniqueServiceNamesList.contains(metricEntry.getServiceName())) {
-                uniqueServiceNamesList.add(metricEntry.getServiceName());
-            }
-        }
+    List<KeplerMetricDTO> keplerMetricData = keplerMetricHandler.getAllKeplerByDateAndTime(
+      from,
+      to,
+      minutesAgo,
+      type,
+      keplerTypeList
+    );
 
+    List<String> uniqueServiceNamesList = new ArrayList<>();
 
-        List<KeplerResponseData> finalResponse = new ArrayList<>();
-
-        // Find By ServiceName from response
-        for (String serviceName : uniqueServiceNamesList) {
-            List<ContainerPowerMetrics> containerPowerMetricsList = new ArrayList<>();
-            for (KeplerMetricDTO entry : resDto) {
-                if (entry.getServiceName().equals(serviceName)) {
-                    ContainerPowerMetrics containerPowerMetrics = new ContainerPowerMetrics(entry.getDate(),
-                            entry.getPowerConsumption());
-                    containerPowerMetricsList.add(containerPowerMetrics);
-                }
-            }
-            KeplerResponseData keplerResponseData = new KeplerResponseData(serviceName, containerPowerMetricsList);
-            finalResponse.add(keplerResponseData);
-        }
-
-        return Response.ok(finalResponse).build();
-
+    for (KeplerMetricDTO metricEntry : keplerMetricData) {
+      if (!uniqueServiceNamesList.contains(metricEntry.getServiceName())) {
+        uniqueServiceNamesList.add(metricEntry.getServiceName());
+      }
     }
 
-    @GET
-    @Path("/getAllKepler-MetricData")
-    public Response getAllKeplerMetricDatas(
-            @QueryParam("from") LocalDate from,
-            @QueryParam("to") LocalDate to,
-            @QueryParam("minutesAgo") int minutesAgo,
-            @QueryParam("type") String type,
-            @QueryParam("keplerType") List<String> keplerTypeList) {
-                LocalDateTime Time1 = LocalDateTime.now();
+    List<String> matchedSystemEntries = new ArrayList<>();
 
-        List<KeplerMetricDTO> keplerMetricData = keplerMetricHandler.getAllKeplerByDateAndTime(from, to, minutesAgo,
-                type,keplerTypeList);
-
-
-
-        LocalDateTime Time5 = LocalDateTime.now();
-        List<String> uniqueServiceNamesList = new ArrayList<>();
-
-        for (KeplerMetricDTO metricEntry : keplerMetricData) {
-            if (!uniqueServiceNamesList.contains(metricEntry.getServiceName())) {
-                uniqueServiceNamesList.add(metricEntry.getServiceName());
-            }
-        }
-
-        List<String> matchedSystemEntries = new ArrayList<>();
-
-        // Identify all items starting with "system" and store their indices
-        for (int i = 0; i < uniqueServiceNamesList.size(); i++) {
-            if (uniqueServiceNamesList.get(i).startsWith("system")) {
-                matchedSystemEntries.add(uniqueServiceNamesList.get(i));
-            }
-        }
-
-        // Move all matched entries to the front of the list
-        for (String matchedEntry : matchedSystemEntries) {
-            uniqueServiceNamesList.remove(matchedEntry); 
-            uniqueServiceNamesList.add(0, matchedEntry); 
-        }
-
-
-        List<KeplerResponseData> finalResponse = new ArrayList<>();
-
-        // Find By ServiceName from response
-        for (String serviceName : uniqueServiceNamesList) {
-            List<ContainerPowerMetrics> containerPowerMetricsList = new ArrayList<>();
-            for (KeplerMetricDTO entry : keplerMetricData) {
-                if (entry.getServiceName().equals(serviceName)) {
-                    ContainerPowerMetrics containerPowerMetrics = new ContainerPowerMetrics(entry.getDate(),
-                            entry.getPowerConsumption());
-                    containerPowerMetricsList.add(containerPowerMetrics);
-                }
-            }
-            KeplerResponseData keplerResponseData = new KeplerResponseData(serviceName, containerPowerMetricsList);
-            finalResponse.add(keplerResponseData);
-        }
-        
-             LocalDateTime Time6 = LocalDateTime.now();
-            System.out.println("Mapping logic ended Timestamp------ " + Duration.between(Time5, Time6));
-
-
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            String responseJson = objectMapper.writeValueAsString(finalResponse);
-            LocalDateTime Time2 = LocalDateTime.now();
-            System.out.println("API ended Timestamp------ " + Duration.between(Time1, Time2));
-            return Response.ok(responseJson, MediaType.APPLICATION_JSON).build();
-        } catch (Exception e) {
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .entity("Error converting response to JSON")
-                    .build();
-        }
+    // Identify all items starting with "system" and store their indices
+    for (int i = 0; i < uniqueServiceNamesList.size(); i++) {
+      if (uniqueServiceNamesList.get(i).startsWith("system")) {
+        matchedSystemEntries.add(uniqueServiceNamesList.get(i));
+      }
     }
 
+    // Move all matched entries to the front of the list
+    for (String matchedEntry : matchedSystemEntries) {
+      uniqueServiceNamesList.remove(matchedEntry);
+      uniqueServiceNamesList.add(0, matchedEntry);
+    }
+
+    List<KeplerResponseData> finalResponse = new ArrayList<>();
+
+    // Find By ServiceName from response
+    for (String serviceName : uniqueServiceNamesList) {
+      List<ContainerPowerMetrics> containerPowerMetricsList = new ArrayList<>();
+      for (KeplerMetricDTO entry : keplerMetricData) {
+        if (entry.getServiceName().equals(serviceName)) {
+          ContainerPowerMetrics containerPowerMetrics = new ContainerPowerMetrics(
+            entry.getDate(),
+            entry.getPowerConsumption()
+          );
+          containerPowerMetricsList.add(containerPowerMetrics);
+        }
+      }
+      KeplerResponseData keplerResponseData = new KeplerResponseData(
+        serviceName,
+        containerPowerMetricsList
+      );
+      finalResponse.add(keplerResponseData);
+    }
+
+    try {
+      ObjectMapper objectMapper = new ObjectMapper();
+      String responseJson = objectMapper.writeValueAsString(finalResponse);
+
+      LocalDateTime APICallEnd = LocalDateTime.now();
+
+      System.out.println(
+        "------------API call endTimestamp------ " + APICallEnd
+      );
+
+      System.out.println("-----------API call duration------- " +(Duration.between(APICallStart, APICallEnd)));
+
+      return Response.ok(responseJson, MediaType.APPLICATION_JSON).build();
+    } catch (Exception e) {
+      return Response
+        .status(Response.Status.INTERNAL_SERVER_ERROR)
+        .entity("Error converting response to JSON")
+        .build();
+    }
+  }
 }
--- src/main/java/com/zaga/handler/KeplerMetricHandler.java
+++ src/main/java/com/zaga/handler/KeplerMetricHandler.java
@@ -1,26 +1,18 @@
 package com.zaga.handler;
 
-import com.mongodb.client.AggregateIterable;
 import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
 import com.zaga.entity.queryentity.kepler.KeplerMetricDTO;
-import com.zaga.entity.queryentity.kepler.KeplerMetricQuery;
 import com.zaga.repo.KeplerMetricRepo;
-
 import io.quarkus.mongodb.panache.PanacheQuery;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
@@ -33,73 +25,112 @@ public class KeplerMetricHandler {
   @Inject
   MongoClient mongoClient;
 
-
-  public List<KeplerMetricQuery> getKeplerData(  ) {
-    MongoCollection<Document> collection = mongoClient
-      .getDatabase("KeplerMetric")
-      .getCollection("Metrics");
-    // Bson matchStage;
-
-    List<Bson> pipeline = Arrays.asList(
-      new Document(
-        "$project",
-        new Document("resourceMetrics.scopeMetrics.metrics", 1L)
-      ),
-      new Document("$unwind", new Document("path", "$resourceMetrics")),
-      new Document(
-        "$unwind",
-        new Document("path", "$resourceMetrics.scopeMetrics")
-      ),
-      new Document(
-        "$unwind",
-        new Document("path", "$resourceMetrics.scopeMetrics.metrics")
-      ),
-      new Document(
-        "$match",
-        new Document(
-          "resourceMetrics.scopeMetrics.metrics.name",
-          "kepler_container_cpu_cycles_total"
-        )
-      ),
-      new Document(
-        "$unwind",
-        new Document(
-          "path",
-          "$resourceMetrics.scopeMetrics.metrics.sum"
-        )
-      )
-    //   new Document(
-    //     "$unwind",
-    //     new Document(
-    //       "path",
-    //       "$resourceMetrics.scopeMetrics.metrics.sum.dataPoints.attributes"
-    //     )
-    //   )
-    );
-    AggregateIterable<Document> aggregationResult = collection.aggregate(
-      pipeline
-    );
-
-    List<KeplerMetricQuery> keplerMetrics = new ArrayList<>();
-    for(Document document : aggregationResult){
-        // System.out.println("Aggregation result:===================================== " + document.get("dataPoints").toString());
+  public List<KeplerMetricDTO> getAllKeplerByDateAndTime(
+    LocalDate from,
+    LocalDate to,
+    int minutesAgo,
+    String type,
+    List<String> keplerTypeList
+  ) {
+    Document timeFilter;
+    if (from != null && to != null && to.isBefore(from)) {
+      LocalDate temp = from;
+      from = to;
+      to = temp;
     }
-    
 
-    // Return the list of KeplerMetricQuery objects
-    return keplerMetrics;
+    if (from != null && to != null) {
+      timeFilter = createCustomDateFilter(from, to);
+    } else if (minutesAgo > 0) {
+      LocalDateTime currentDateTime = LocalDateTime.now();
+      LocalDateTime startOfToday = LocalDate.now().atStartOfDay();
+
+      LocalDateTime fromDateTime = currentDateTime.minusMinutes(minutesAgo);
+      if (fromDateTime.isBefore(startOfToday)) {
+        fromDateTime = startOfToday;
+      }
+      LocalDateTime toDateTime = currentDateTime;
+
+      LocalDateTime DBCallOneStart = LocalDateTime.now();
+      
+      System.out.println(
+        "------------DB call One startTimestamp------ " + DBCallOneStart
+      );
+
+      Bson bsonFilter = Filters.and(
+        Filters.gte(
+          "date",
+          Date.from(fromDateTime.atZone(ZoneId.systemDefault()).toInstant())
+        ),
+        Filters.lt(
+          "date",
+          Date.from(toDateTime.atZone(ZoneId.systemDefault()).toInstant())
+        )
+      );
+      
+      LocalDateTime DBCallOneEnd = LocalDateTime.now();
+
+      System.out.println(
+        "------------DB call One endTimestamp------ " + DBCallOneEnd
+      );
+
+      System.out.println(
+        "-----------DB call ended Timestamp------ " +
+        Duration.between(DBCallOneStart, DBCallOneEnd)
+      );
+
+ 
+      timeFilter = Document.parse(bsonFilter.toBsonDocument().toJson());
+
+    } else {
+      throw new IllegalArgumentException(
+        "Either date range or minutesAgo must be provided"
+      );
+    }
+
+    if (type != null && !type.isEmpty()) {
+      timeFilter.append("type", type);
+    }
+
+    if (keplerTypeList != null && !keplerTypeList.isEmpty()) {
+      timeFilter.append("keplerType", new Document("$in", keplerTypeList));
+    }
+
+    LocalDateTime DBCallTwoStart = LocalDateTime.now();
+
+    System.out.println(
+      "------------DB call Two startTimestamp------ " + DBCallTwoStart
+    );
+
+    PanacheQuery<KeplerMetricDTO> query = keplerMetricRepo.find(timeFilter);
+
+    LocalDateTime DBCallTwoEnd = LocalDateTime.now();
+
+    System.out.println(
+      "------------DB call Two endTimestamp------ " + DBCallTwoEnd
+    );
+
+    System.out.println(
+      "-----------DB call ended Timestamp------ " +
+      Duration.between(DBCallTwoStart, DBCallTwoEnd)
+    );
+
+    return query.list();
   }
 
+  private Document createCustomDateFilter(LocalDate from, LocalDate to) {
+    Bson bsonFilter = Filters.and(
+      Filters.gte("date", from.atStartOfDay()),
+      Filters.lt("date", to.plusDays(1).atStartOfDay())
+    );
 
-
-
-  //get all kepler metric dto 
+    return Document.parse(bsonFilter.toBsonDocument().toJson());
+  }
+}
+//get all kepler metric dto
 //   public List<KeplerMetricDTO> getAllKeplerByMinutes(LocalDate from, LocalDate to, int minutesAgo) {
 //    return keplerMetricRepo.listAll();
 // }
-
-
-
 // public List<KeplerMetricDTO> getAllKeplerByDateAndTime(LocalDate from, LocalDate to, int minutesAgo) {
 //     Document timeFilter;
 //     if (from != null && to != null && to.isBefore(from)) {
@@ -107,43 +138,34 @@ public class KeplerMetricHandler {
 //         from = to;
 //         to = temp;
 //     }
-
 //     if (from != null && to != null) {
 //         timeFilter = createCustomDateFilter(from, to);
 //     } else if (minutesAgo > 0) {
 //         LocalDateTime currentDateTime = LocalDateTime.now();
 //         LocalDateTime startOfToday = LocalDate.now().atStartOfDay();
-
 //         LocalDateTime fromDateTime = currentDateTime.minusMinutes(minutesAgo);
 //         if (fromDateTime.isBefore(startOfToday)) {
 //             fromDateTime = startOfToday;
 //         }
 //         LocalDateTime toDateTime = currentDateTime;
-
 //         Bson bsonFilter = Filters.and(
 //                 Filters.gte("date", fromDateTime),
 //                 Filters.lt("date", toDateTime)
 //         );
-
 //      timeFilter = Document.parse(bsonFilter.toBsonDocument().toJson());
 //     } else {
 //         throw new IllegalArgumentException("Either date range or minutesAgo must be provided");
 //     }
-
 //     PanacheQuery<KeplerMetricDTO> query = keplerMetricRepo.find(timeFilter);
 //     return query.list();
 // }
-
 // private Document createCustomDateFilter(LocalDate from, LocalDate to) {
 //     Bson bsonFilter = Filters.and(
 //             Filters.gte("date", from.atStartOfDay()),
 //             Filters.lt("date", to.plusDays(1).atStartOfDay())
 //     );
-
 //    return Document.parse(bsonFilter.toBsonDocument().toJson());
 // }
-
-
 // public List<KeplerMetricDTO> getAllKeplerByDateAndTime(LocalDate from, LocalDate to, int minutesAgo) {
 //  Document timeFilter;
 //     if (from != null && to != null && to.isBefore(from)) {
@@ -151,101 +173,31 @@ public class KeplerMetricHandler {
 //         from = to;
 //         to = temp;
 //     }
-
 //     if (from != null && to != null) {
 //         timeFilter = createCustomDateFilter(from, to);
 //     } else if (minutesAgo > 0) {
 //         LocalDateTime currentDateTime = LocalDateTime.now();
 //         LocalDateTime startOfToday = LocalDate.now().atStartOfDay();
-
 //         LocalDateTime fromDateTime = currentDateTime.minusMinutes(minutesAgo);
 //         if (fromDateTime.isBefore(startOfToday)) {
 //             fromDateTime = startOfToday;
 //         }
 //         LocalDateTime toDateTime = currentDateTime;
-
 //         Bson bsonFilter = Filters.and(
 //                 Filters.gte("date", Date.from(fromDateTime.atZone(ZoneId.systemDefault()).toInstant())),
 //                 Filters.lt("date", Date.from(toDateTime.atZone(ZoneId.systemDefault()).toInstant()))
 //         );
-
 //         timeFilter = Document.parse(bsonFilter.toBsonDocument().toJson());
 //     } else {
 //         throw new IllegalArgumentException("Either date range or minutesAgo must be provided");
 //     }
-
 //     PanacheQuery<KeplerMetricDTO> query = keplerMetricRepo.find(timeFilter);
 //     return query.list();
 // }
-
 // private Document createCustomDateFilter(LocalDate from, LocalDate to) {
 //     Bson bsonFilter = Filters.and(
 //             Filters.gte("date", from.atStartOfDay()),
 //             Filters.lt("date", to.plusDays(1).atStartOfDay())
 //     );
-
 //     return Document.parse(bsonFilter.toBsonDocument().toJson());
 // }
-
-
-public List<KeplerMetricDTO> getAllKeplerByDateAndTime(LocalDate from, LocalDate to, int minutesAgo, String type, List<String> keplerTypeList) {
-
-  LocalDateTime Time3 = LocalDateTime.now();
- Document timeFilter;
-    if (from != null && to != null && to.isBefore(from)) {
-        LocalDate temp = from;
-        from = to;
-        to = temp;
-    }
-
-    if (from != null && to != null) {
-        timeFilter = createCustomDateFilter(from, to);
-    } else if (minutesAgo > 0) {
-        LocalDateTime currentDateTime = LocalDateTime.now();
-        LocalDateTime startOfToday = LocalDate.now().atStartOfDay();
-
-        LocalDateTime fromDateTime = currentDateTime.minusMinutes(minutesAgo);
-        if (fromDateTime.isBefore(startOfToday)) {
-            fromDateTime = startOfToday;
-        }
-        LocalDateTime toDateTime = currentDateTime;
-
-        Bson bsonFilter = Filters.and(
-                Filters.gte("date", Date.from(fromDateTime.atZone(ZoneId.systemDefault()).toInstant())),
-                Filters.lt("date", Date.from(toDateTime.atZone(ZoneId.systemDefault()).toInstant()))
-        );
-
-        timeFilter = Document.parse(bsonFilter.toBsonDocument().toJson());
-    } else {
-        throw new IllegalArgumentException("Either date range or minutesAgo must be provided");
-    }
-
-  if (type != null && !type.isEmpty()) {
-      timeFilter.append("type", type);
-  }
-
-
-  if (keplerTypeList != null && !keplerTypeList.isEmpty()) {
-    timeFilter.append("keplerType", new Document("$in", keplerTypeList));
-}
-
-  PanacheQuery<KeplerMetricDTO> query = keplerMetricRepo.find(timeFilter);
-
-  
-   LocalDateTime Time4 = LocalDateTime.now();
-  
-   System.out.println("DB call ended Timestamp------ " + Duration.between(Time3, Time4));
-  return query.list();
-}
-
-private Document createCustomDateFilter(LocalDate from, LocalDate to) {
-  Bson bsonFilter = Filters.and(
-          Filters.gte("date", from.atStartOfDay()),
-          Filters.lt("date", to.plusDays(1).atStartOfDay())
-  );    
-
-  return Document.parse(bsonFilter.toBsonDocument().toJson());
-}
-
-
-}
